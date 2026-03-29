5a82276f024967240fcaa017e317021180f09b27
Beam <beamleung@msn.cn>
2026-03-29 16:46:22 +0800

    feat(knowledge-graph): export exceptions from module __init__
    - Add all custom exceptions to __init__.py exports
    - Enable easier imports: from quickagents.knowledge_graph import NodeNotFoundError
    - Follow Python package best practices

--- quickagents/knowledge_graph/__init__.py
+++ quickagents/knowledge_graph/__init__.py
@@ -1,6 +1,19 @@
 """Knowledge Graph module for QuickAgents."""
 
 from .types import NodeType, EdgeType, KnowledgeNode, KnowledgeEdge, SearchResult
+from .exceptions import (
+    KnowledgeGraphError,
+    NodeNotFoundError,
+    EdgeNotFoundError,
+    DuplicateNodeError,
+    DuplicateEdgeError,
+    InvalidNodeTypeError,
+    InvalidEdgeTypeError,
+    CircularDependencyError,
+    DatabaseIntegrityError,
+    ExtractionError,
+    SyncError,
+)
 
 # Note: KnowledgeGraph will be added in Part 3
 # from .knowledge_graph import KnowledgeGraph
@@ -11,5 +24,17 @@ __all__ = [
     'KnowledgeNode',
     'KnowledgeEdge',
     'SearchResult',
+    # Exceptions
+    'KnowledgeGraphError',
+    'NodeNotFoundError',
+    'EdgeNotFoundError',
+    'DuplicateNodeError',
+    'DuplicateEdgeError',
+    'InvalidNodeTypeError',
+    'InvalidEdgeTypeError',
+    'CircularDependencyError',
+    'DatabaseIntegrityError',
+    'ExtractionError',
+    'SyncError',
     # 'KnowledgeGraph',  # Will be added in Part 3
 ]
